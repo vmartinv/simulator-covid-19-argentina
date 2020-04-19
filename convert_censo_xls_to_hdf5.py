f65544fc508b9095f7a27de202c977f7513f7cbe
Martín Villagra <vmartinv@fb.com>
2020-04-19 14:25:36 +0100

    use sylk parser

--- convert_censo_xls_to_hdf5.py
+++ convert_censo_xls_to_hdf5.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import xlrd
+from sylk_parser import SylkParser
 import os
 import glob
 import pandas as pd
@@ -18,7 +18,7 @@ def format_col(val):
     return re.sub(r' +', ' ', re.sub(r'(\d+)\.0', r'\1', val))
 
 def parse_xls(xls):
-    worksheet = xlrd.open_workbook(xls).sheet_by_index(0)
+    worksheet = SylkParser(xls)
     SUBTABLE_START = "AREA"
     SUBTABLE_END = "Total"
     SUBTABLE_EMPTY = "Tabla vac"
@@ -26,37 +26,39 @@ def parse_xls(xls):
     dataset = {}
     cross_cols = None
     current_index = None
-    for row in range(worksheet.nrows):
-        first_col = str(worksheet.cell(row, 0).value).strip()
+    previous_row = None
+    for i, row in enumerate(worksheet):
+        first_col = str(row[0]).strip()
         if area_row is None:
             if first_col.startswith(SUBTABLE_START):
                 number = int(re.match(fr"{SUBTABLE_START}\s*#\s*(\d+)\s*", first_col).group(1))
                 current_index = number
-                name = worksheet.cell(row, 1).value
-                area_row = row
+                name = row[1]
+                area_row = i
                 header_row = None
         else:
             if first_col == SUBTABLE_END or first_col.startswith(SUBTABLE_EMPTY):
                 if current_index not in dataset:
                     dataset[current_index] = {}
                 area_row = None
-            elif row > area_row + 2 and first_col: # actual rows
+            elif i > area_row + 2 and first_col: # actual rows
                 if current_index not in dataset:
                     dataset[current_index] = {}
-                    header_row = row - 1
-                    first_header = str(worksheet.cell(header_row, 1).value).strip()
+                    header_row = previous_row
+                    first_header = str(header_row[1]).strip()
                     if first_header != 'Casos':
                         cross_cols = []
-                        while str(worksheet.cell(header_row, 1+len(cross_cols)).value).strip() != 'Total':
-                            cross_cols.append('.'+format_col(str(worksheet.cell(header_row, 1+len(cross_cols)).value)))
+                        while str(header_row[1+len(cross_cols)]).strip() != 'Total':
+                            cross_cols.append('.'+format_col(header_row[1+len(cross_cols)]))
                     else:
                         cross_cols = ['']
-                for i,col in enumerate(cross_cols):
-                    value = str(worksheet.cell(row, 1+i).value)
+                for j,col in enumerate(cross_cols):
+                    value = row[1+j]
                     if not value:
                         print(f"cannot parse row {row}")
                         exit(1)
                     dataset[current_index][format_col(first_col)+col] = value
+        previous_row = row
     if len(dataset) < 100:
         return None
     dataset_fixed = {INDEX: []}
